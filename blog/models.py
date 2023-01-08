a92d6594e955b891010dfa1f0642ee97d7021a25
tedha012 <tedha012@gmail.com>
2023-01-08 15:11:12 +0900

    modified a Post model added a author field

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 
@@ -6,13 +7,17 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
+
     head_image = models.ImageField(upload_to="blog/images/%Y/%m/%d/", blank=True)
     file_upload = models.FileField(upload_to="blog/files/%Y/%m/%d/", blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self) -> str:
-        return f"[{self.pk}] {self.title}"
+        return f"[{self.pk}] {self.title} :: {self.author}"
 
     def get_absolute_url(self):
         return f"/blog/{self.pk}/"
